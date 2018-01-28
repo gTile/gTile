effca76182003ea2d6a4840284ce0070c5b9de83
Red Daly <reddaly@gmail.com>
2018-01-27 18:33:40 -0800

    WIP on tilespec.ts

--- tilespec.ts
+++ tilespec.ts
@@ -4,17 +4,31 @@
  *
  * The syntax
  */
-export class TileSpec {
+class TileSpec {
+    rows: Number;
+    cols: Number
+    luc: XY;
+    rdc: XY;
+
+    //    constructor(x: Number, y: Number, lucX: Number, lucY: Number, rdcX: Number, rdcY: Number) {
+    constructor(rows: Number, cols: Number, luc, rdc: XY) {
+        this.rows = rows
+        this.cols = cols;
+        this.luc = luc;
+        this.rdc = rdc;
+    }
+}
+
+class XY {
     x: Number;
     y: Number;
-    lucX: Number;
-    lucY: Number;
-    rdcX: Number;
-    rdcY: Number;
+    constructor(x, y: Number) {
+        this.x, this.y = x, y;
+    }
 }
 
 /** parsePreset parses a string like "0x0" */
-export function parsePreset(preset: string) {
+function parsePreset(preset: string) {
     const ps = preset.split(" ");
     if(ps.length != 3) {
         throw new Error("Bad preset: " + preset);
@@ -23,18 +37,16 @@ export function parsePreset(preset: string) {
     const luc = parseTuple(ps[1], ":");
     const rdc = parseTuple(ps[2], ":");
 
-    //log("Parsed " + grid_format.X + "x" + grid_format.Y + " "
-        //+ luc.X + ":" + luc.Y + " " + rdc.X + ":" + rdc.Y);
-    if  (  grid_format.X < 1 || luc.X < 0 || rdc.X < 0
-        || grid_format.Y < 1 || luc.Y < 0 || rdc.Y < 0
-        || grid_format.X <= luc.X || grid_format.X <= rdc.X
-        || grid_format.Y <= luc.Y || grid_format.Y <= rdc.Y
-        || luc.X > rdc.X || luc.Y > rdc.Y) {
+    //log("Parsed " + grid_format.x + "x" + grid_format.y + " "
+        //+ luc.x + ":" + luc.y + " " + rdc.x + ":" + rdc.y);
+    if  (  grid_format.x < 1 || luc.x < 0 || rdc.x < 0
+        || grid_format.y < 1 || luc.y < 0 || rdc.y < 0
+        || grid_format.x <= luc.x || grid_format.x <= rdc.x
+        || grid_format.y <= luc.y || grid_format.y <= rdc.y
+        || luc.x > rdc.x || luc.y > rdc.y) {
         throw new Error("Bad preset: " + preset);
     }
-    const result = new TileSpec();
-    result.x = parseInt('10');
-    return result;
+    return new TileSpec(grid_format.x, grid_format.y, luc, rdc);
 }
 
 function parseTuple(format, delimiter) {
@@ -45,6 +57,5 @@ function parseTuple(format, delimiter) {
         || isNaN(gssk[1]) || gssk[1] < 0 || gssk[1] > 40) {
         throw new Error("Bad format " + format + ", delimiter " + delimiter);
     }
-    //log("Parsed format " + gssk[0] + delimiter + gssk[1]);
-    return {X: Number(gssk[0]), Y: Number(gssk[1]) };
+    return new XY(Number(gssk[0]), Number(gssk[1]));
 }
