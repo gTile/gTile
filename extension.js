2c018c78e28f16e4f0e7755836354cba800b24ce
Sergey Cherepanov <sergey@cherepanov.org>
2020-04-10 23:20:29 -0500

    Update extension version, fix missing close button

--- extension.js
+++ extension.js
@@ -1221,10 +1221,11 @@ TopBar.prototype = {
 
         this._stlabel =  new St.Label({style_class: 'grid-title', text: this._title});
 
-        this._closebutton = new GTileStatusButton('close-button');
-        this._closebutton.container.add_style_class_name('close-button-container');
+        this._closebutton = new St.Button({style_class: 'close-button'})
+        this._closebutton.add_style_class_name('close-button-container');
+        this._connect_id = this._closebutton.connect('button-press-event', Lang.bind(this, this._onButtonPress));
 
-        this.actor.add_actor(this._closebutton.container);
+        this.actor.add_actor(this._closebutton);
         this.actor.add_actor(this._stlabel);
 
     },
@@ -1239,6 +1240,16 @@ TopBar.prototype = {
         log("title: "+this._title);
         this._stlabel.text = this._title;
 
+    },
+
+    _onButtonPress() {
+      log("Close button");
+      toggleTiling();
+    },
+
+    destroy() {
+      this._closebutton.disconnect(this._connect_id);
+      super.destroy();
     },
 };
 
