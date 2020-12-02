99ac66a9d2289974c0771d4490c8afa87686a2fb
JShackelford <5092454+jshack88@users.noreply.github.com>
2020-12-02 13:36:11 -0700

    Fix Copy Paste Error in window.move_frame

--- app.ts
+++ app.ts
@@ -617,7 +617,7 @@ function move_resize_window_with_margins(metaWindow, x, y, width, height){
     height = height - 2 * vBorderY ;
     log("After margins and visible border window is " + x + ":" + y + " - " + width + ":" + height);
 
-    metaWindow.move_frame(true, x, y, width, height);
+    metaWindow.move_frame(true, x, y);
     metaWindow.move_resize_frame(true, x, y, width, height);
 }
 
