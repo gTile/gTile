67aac70eb69422dbebdcdccaad02c2597df3efa6
Karolis Sakavicius <karolis.sakavicius@toptal.com>
2021-12-28 21:27:45 +0200

    Add visual feedback when changing grid size

--- app.ts
+++ app.ts
@@ -38,6 +38,7 @@ const Meta = imports.gi.Meta;
 const Clutter = imports.gi.Clutter;
 const Signals = imports.signals;
 const Workspace = imports.ui.workspace;
+const Mainloop = imports.mainloop;
 // Getter for accesing "get_active_workspace" on GNOME <=2.28 and >= 2.30
 const WorkspaceManager: WorkspaceManagerInterface = (
     global.screen || global.workspace_manager);
@@ -66,6 +67,7 @@ const SETTINGS_WINDOW_MARGIN = 'window-margin';
 const SETTINGS_WINDOW_MARGIN_FULLSCREEN_ENABLED = 'window-margin-fullscreen-enabled';
 const SETTINGS_MAX_TIMEOUT = 'max-timeout';
 const SETTINGS_MAIN_WINDOW_SIZES = 'main-window-sizes';
+const SETTINGS_SHOW_GRID_LINES = 'show-grid-lines';
 
 const SETTINGS_INSETS_PRIMARY = 'insets-primary';
 const SETTINGS_INSETS_PRIMARY_LEFT = 'insets-primary-left';
@@ -94,6 +96,7 @@ interface ParsedSettings {
     [SETTINGS_WINDOW_MARGIN_FULLSCREEN_ENABLED]: boolean;
     [SETTINGS_MAX_TIMEOUT]: any;
     [SETTINGS_MAIN_WINDOW_SIZES]: Array<string>;
+    [SETTINGS_SHOW_GRID_LINES]: boolean;
 
     [SETTINGS_INSETS_PRIMARY]: number;
     [SETTINGS_INSETS_PRIMARY_LEFT]: number;
@@ -122,6 +125,7 @@ const gridSettings: ParsedSettings = {
     [SETTINGS_WINDOW_MARGIN_FULLSCREEN_ENABLED]: false,
     [SETTINGS_MAX_TIMEOUT]: null,
     [SETTINGS_MAIN_WINDOW_SIZES]: [],
+    [SETTINGS_SHOW_GRID_LINES]: false,
 
     [SETTINGS_INSETS_PRIMARY]: 0,
     [SETTINGS_INSETS_PRIMARY_LEFT]: 0,
@@ -377,6 +381,10 @@ class App {
         nbCols = gridSize.width;
         nbRows = gridSize.height;
         this.refreshGrids();
+
+        if (gridSettings[SETTINGS_SHOW_GRID_LINES]) {
+            this._showGridLines(gridSize);
+        }
     }
 
     moveGrids() {
@@ -604,6 +612,45 @@ class App {
         return this.gridShowing;
     }
 
+    _showGridLines(gridSize: tilespec.GridSize): void {
+        log("Started drawing grid lines...");
+
+        let gridTiles:Array<StBoxLayout> = [];
+
+        for(let monitorIdx = 0; monitorIdx < activeMonitors().length; monitorIdx++) {
+            const workArea = workAreaRectByMonitorIndex(monitorIdx);
+            const monitor = activeMonitors()[monitorIdx];
+
+            if (!workArea) { continue; }
+
+            let tileHeight = workArea.size.height / gridSize.height;
+            let tileWidth = workArea.size.width / gridSize.width;
+            let topOffset = workArea.topLeft().y;
+            let leftOffset = workArea.topLeft().x;
+
+            for(let i = 1; i <= gridSize.width; i++) {
+                for(let u = 1; u <= gridSize.height; u++) {
+                    const newGridWidget = new St.BoxLayout({ style_class: `${theme}__grid_lines_preview` });
+                    gridTiles.push(newGridWidget);
+
+                    Main.uiGroup.add_actor(newGridWidget);
+
+                    newGridWidget.width = tileWidth;
+                    newGridWidget.height = tileHeight;
+                    newGridWidget.x = leftOffset + tileWidth * (i - 1);
+                    newGridWidget.y = topOffset + tileHeight * (u - 1);
+                }
+            }
+        }
+
+        Mainloop.timeout_add(1000, () => {
+            log("Removing grid lines...");
+            for(let tile of gridTiles) {
+                Main.uiGroup.remove_actor(tile);
+            }
+        });
+    }
+
     /**
      * onFocus is called when the global focus changes.
      */
@@ -767,6 +814,7 @@ function initSettings() {
     getBoolSetting(SETTINGS_GLOBAL_PRESETS);
     getBoolSetting(SETTINGS_TARGET_PRESETS_TO_MONITOR_OF_MOUSE);
     getBoolSetting(SETTINGS_MOVERESIZE_ENABLED);
+    getBoolSetting(SETTINGS_SHOW_GRID_LINES);
 
     gridSettings[SETTINGS_WINDOW_MARGIN] = getIntSetting(SETTINGS_WINDOW_MARGIN);
     gridSettings[SETTINGS_WINDOW_MARGIN_FULLSCREEN_ENABLED] = getBoolSetting(SETTINGS_WINDOW_MARGIN_FULLSCREEN_ENABLED);
