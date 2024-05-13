06d58523c01780193e26ac292ef9f6700a114fc8
Markus Schanz <markus@schanz.id>
2024-05-13 20:34:37 +0200

    Update @girs/* dependencies

--- gnome-shell.d.ts
+++ gnome-shell.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="./node_modules/@girs/gjs/dom.d.ts" />
 /// <reference path="./node_modules/@girs/gjs/gjs.d.ts" />
 /// <reference path="./node_modules/@girs/gnome-shell/dist/index-ambient.d.ts" />
-/// <reference path="./node_modules/@girs/gnome-shell/src/extensions/global.d.ts" />
+/// <reference path="./node_modules/@girs/gnome-shell/dist/extensions/global.d.ts" />
