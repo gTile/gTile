852ac726cab8bcc11b66ae663ee5bb7bf0997359
Fernn <74260644+itsfernn@users.noreply.github.com>
2025-11-11 11:30:38 +0100

    Remove "always above" exception from autogrow

--- src/core/DesktopManager.ts
+++ src/core/DesktopManager.ts
@@ -304,7 +304,6 @@ export default class implements Publisher<DesktopEvent>, GarbageCollector {
     const collisionWindows = workspace.list_windows().filter(win => !(
       win === target ||
       win.minimized ||
-      win.is_above() ||
       win.get_frame_type() !== Meta.FrameType.NORMAL ||
       TitleBlacklist.some(p => p.test(win.title ?? "")) ||
       win.get_monitor() !== monitorIdx ||
